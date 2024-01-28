e5624151529994c4b09ef7843560d54b3cca801d
filipgrabowiecki <150305613+filipgrabowiecki@users.noreply.github.com>
2024-01-28 19:56:37 +0100

    skończony projekt

--- drone_controller/drone_controller/drone_project.py
+++ drone_controller/drone_controller/drone_project.py
@@ -7,7 +7,9 @@ from geometry_msgs.msg import Twist, Pose
 class DroneController(Node):
     def __init__(self):
         super().__init__('drone_controller')
-        
+        self.a = True
+        self.x = 1.0
+        self.y = 1.0
         # Current pose subscriber
         self.gt_pose_sub = self.create_subscription(
             Pose,
@@ -19,27 +21,51 @@ class DroneController(Node):
 
         # Control command publisher
         self.command_pub = self.create_publisher(Twist, '/drone/cmd_vel', 10)
-        
+
         # Callback for executing a control commands
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
         # Feel fre to fill with your code! Add some objects to represent a goal points to achieve
 
-    
     def pose_callback(self, data):
         self.gt_pose = data
         print(f"{data}")
 
-    
     def timer_callback(self):
         # HINT: Check a current pose. Use it to check if a drone achieved a desired pose.
         print(f"Current pose: {self.gt_pose}")
-        
         # HINT: Use a self.command_pub to publish a command
+        one = [1.0, 1.0]
+        two = [1.0, 5.0]
+        three = [5.0, 5.0]
+        four = [5.0, 1.0]
+        msg = Twist()
+        msg.linear.z = 7.0
+        xpos = 0.0
+        ypos = 0.0
+        if self.gt_pose is not None:
+            xpos = self.gt_pose.position.x
+            ypos = self.gt_pose.position.y
+        if xpos < 1.1 and ypos < 1.1:
+            self.x = two[0]
+            self.y = two[1]
+            b = 1
+        if xpos < 1.1 and ypos > 4.9:
+            self.x = three[0]
+            self.y = three[1]
+        if xpos > 4.9 and ypos > 4.9:
+            self.x = four[0]
+            self.y = four[1]
+        if xpos > 4.9 and ypos < 1.1:
+            self.x = one[0]
+            self.y = one[1]
+
+        msg.linear.x = self.x
+        msg.linear.y = self.y
+        self.command_pub.publish(msg)
         # Fill with your code!
         print("Published!")
-
 
 
 def main(args=None):
@@ -51,6 +77,7 @@ def main(args=None):
 
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
